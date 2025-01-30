4a1891d659800b64b433d9e48c37cbb5ddbb6a30
Edoardo Dottori <edottori17@gmail.com>
2025-01-30 19:06:12 +0100

    Rimosse credenziali sensibili

--- Models/User.cs
+++ Models/User.cs
@@ -1,11 +1,18 @@
-﻿namespace TopSecret.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TopSecret.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
-        public string Username { get; set; }
-        public string Pin { get; set; }
-        public int Points { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public string Username { get; set; } = string.Empty; // Imposta un valore di default
+
+        [Required]
+        public string Pin { get; set; } = string.Empty; // Imposta un valore di default
+
+        public int Points { get; set; } = 0;
     }
 }
